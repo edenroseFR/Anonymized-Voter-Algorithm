2608e2dd91c8d01b81b260fffce28c594ae6ef76
edenroseFR <edenroserisma13@gmail.com>
2022-05-29 05:51:53 +0800

    Delete unused file

--- generate_voter_pkeys.py
+++
@@ -1,9 +0,0 @@
-"""Generate voter's key"""
-
-
-
-def get_voters_key():
-    # generate two large primes
-    # process primes to get public_keys, and private_key
-    # return (pub_keys, priv_key)
-    pass
